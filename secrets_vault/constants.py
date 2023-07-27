67a7ca6010d07a5167614fc351a8b280ac6bc761
anthonynsimon <anthonynsimon@users.noreply.github.com>
2023-07-27 16:24:23 +0200

    When using the edit command, only persist the vault if there were changes (incl. comments) (#8)

--- secrets_vault/constants.py
+++ secrets_vault/constants.py
@@ -1,4 +1,21 @@
+import json
+
+from secrets_vault.backends import AES256GCMBackend
+
 DEFAULT_FILE_FORMAT = "yaml"
 DEFAULT_MASTER_KEY_FILEPATH = "./master.key"
 DEFAULT_SECRETS_FILEPATH = "./secrets.yml.enc"
+DEFAULT_BACKEND = AES256GCMBackend
 UNSET = object()
+
+EXAMPLE_SECRETS_YAML = """
+# Add your secrets below, comments are supported too.
+# app:
+#     secret-key: abc123
+
+database-url: postgres://user:pass@localhost:5432/dev
+""".strip()
+
+EXAMPLE_SECRETS_JSON = json.dumps(
+    {"app": {"secret-key": "abc123"}, "database-url": "postgres://user:pass@localhost:5432/dev"}
+)
